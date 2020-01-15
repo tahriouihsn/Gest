4488dd5a167c773c9ab356d03963f09a6a10b587
tahriouihsn <hfoot0258@gmail.com>
2020-01-15 14:54:28 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ClientsListComponent } from './components/clients-list/clients-list.component';
+import { ClientsAddComponent } from './components/clients-add/clients-add.component';
+import { ClientsEditComponent } from './components/clients-edit/clients-edit.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import {AngularFireModule} from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsListComponent,
+    ClientsAddComponent,
+    ClientsEditComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
